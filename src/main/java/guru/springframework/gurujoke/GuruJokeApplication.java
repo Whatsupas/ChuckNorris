4a24e37eefc0ed953a0a2dc39d05133c24b43abb
RobertasTutlys <robertastut@gmail.com>
2020-03-24 21:12:33 +0100

    Added: changed java configuration to xml configuration.

--- src/main/java/guru/springframework/gurujoke/GuruJokeApplication.java
+++ src/main/java/guru/springframework/gurujoke/GuruJokeApplication.java
@@ -3,8 +3,10 @@ package guru.springframework.gurujoke;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import guru.springframework.gurujoke.services.JokeServiceImpl;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class GuruJokeApplication {
 
     public static void main(String[] args) {
